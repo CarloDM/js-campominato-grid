3a28c2ba3e8f50624e9a7b2404379bc7734ea0c4
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-29 16:56:48 +0200

    trigger button

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-console.log('ciao capominato')
+let boxNumber = 100;
+ // 2. bottone trigger
+btnOne.addEventListener('click', function(){
+  console.log('start!')
 
-// 1. creare l aggiunta dei box
-// 2. bottone trigger
-
-// __ sistema d aggiunta
-const container = document.getElementById('my_container')
-
-console.log(container);
+  // 1. creare l aggiunta dei box
+  const container = document.getElementById('my_container')
+  // reset
+  container.innerHTML ='';
 // ciclo d aggiunta
-for (let i = 1; i < 101; i++) {
+  for (let i = 1; i < boxNumber + 1; i++) {
+    
   idBox = `<div id="${i}" class="box">${i}</div>`
   container.innerHTML += idBox;
-  // console.log(idBox)
 }
 
 
-function boxGeneretor(nBox){
 
-}
+
+
+
+
+
+  
+  console.log('and!')
+})
