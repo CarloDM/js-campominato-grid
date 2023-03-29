f12ac2dedf00008d635addebc6743da42827c199
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-29 17:39:18 +0200

    trigger btn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+
 let boxNumber = 100;
  // 2. bottone trigger
 btnOne.addEventListener('click', function(){
-  console.log('start!')
+  console.log('trig start!')
 
   // 1. creare l aggiunta dei box
   const container = document.getElementById('my_container')
@@ -9,6 +10,7 @@ btnOne.addEventListener('click', function(){
   container.innerHTML ='';
 // ciclo d aggiunta
   for (let i = 1; i < boxNumber + 1; i++) {
+    // generare disposizione i a random      <-------------------
     
   idBox = `<div id="${i}" class="box">${i}</div>`
   container.innerHTML += idBox;
@@ -16,11 +18,15 @@ btnOne.addEventListener('click', function(){
 
 
 
-
-
-
-
-
-  
-  console.log('and!')
+console.log('trig and!')
 })
+
+
+// devo sentire il click in se eseguito sopra ogni div 
+// prendo i div e mi metto ad ascoltarli
+const boxs = document.getElementsByClassName("box");
+console.log(boxs.length)
+
+      boxs.addEventListener("click", function() {
+        console.log('click div ');
+      });
