36156e000b82c4c84ef36679c86b67fd73040fb6
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-30 13:00:37 +0200

    generatoreboxs function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
-
+// ELEMENTI GLOBALI
 let boxNumber = 100;
- // 2. bottone trigger
-btnOne.addEventListener('click', function(){
-  console.log('trig start!')
+let btn = document.getElementById('btnOne')
 
-  // 1. creare l aggiunta dei box
+// 2. bottone trigger
+btn.addEventListener("click", function(){generatoreBoxs()});
+
+function generatoreBoxs() {
+  console.log('generatore box start!')
+  
+    // 1. prendere container
   const container = document.getElementById('my_container')
-  // reset
-  container.innerHTML ='';
-// ciclo d aggiunta
-  for (let i = 1; i < boxNumber + 1; i++) {
-    // generare disposizione i a random      <-------------------
-    
-  idBox = `<div id="${i}" class="box">${i}</div>`
-  container.innerHTML += idBox;
-}
+    // reset
+        container.innerHTML ='';
+  // ciclo d aggiunta dei div al container
+    for (let i = 1; i < boxNumber + 1; i++) {  
+    idBox = `<div id="${i}" class="box">${i}</div>`
+    container.innerHTML += idBox;
+  }
 
-
-
-console.log('trig and!')
-})
+  console.log('generatore box and!')
+  }
 
 
 // devo sentire il click in se eseguito sopra ogni div 
 // prendo i div e mi metto ad ascoltarli
-const boxs = document.getElementsByClassName("box");
-console.log(boxs.length)
+// const boxs = document.getElementsByClassName("box");
+// console.log(boxs.length)
 
-      boxs.addEventListener("click", function() {
-        console.log('click div ');
-      });
+
+      // boxs.addEventListener("click", handleClickBox);
+
+
+// CLICK CELLA
+//  verifico se è una bomba
+/**  si ---> fine gioco
+
+    no ---> aggiungo classe active alla celle
+            punteggio + 1 
+            verificare se punteggio ha raggiunto il massimo --> fine gioco
+ */
+/*
+// FINE GIOCO
+- ACCENDERE TUTTE LE BOMBE
+- stampare msg con punteggio se vinci o perdi
+- congelo la griglia e l opacizzo
+
+*/
+
+// function handleClickBox (){
+//   console.log(this.value)
+// }
